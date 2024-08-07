0b9b5cd1ecd25fad26776a9825d3d2d275248541
Achraf <Achraf@192.168.1.113>
2024-08-07 17:07:10 +0100

    WorkTree

---
+++ LMS2024/src/com/back/TestBack.java
@@ -0,0 +1,10 @@
+package com.back;
+
+public class TestBack {
+
+	public static void main(String[] args) {
+		System.out.println("BackEnd !!");
+
+	}
+
+}
---
+++ LMS2024/src/com/front/TestFront.java
@@ -0,0 +1,10 @@
+package com.front;
+
+public class TestFront {
+
+	public static void main(String[] args) {
+		System.out.println("Front End !!");
+
+	}
+
+}
